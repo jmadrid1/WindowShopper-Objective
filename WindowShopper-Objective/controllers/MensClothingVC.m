a9fac49da6fd4855daee32a9ed21f75742efea9f
Jovel <jovelmadrid@gmail.com>
2019-04-28 13:45:52 -0700

    Refactored Code
    Removed unnecessary UI code

--- WindowShopper-Objective/controllers/MensClothingVC.m
+++ WindowShopper-Objective/controllers/MensClothingVC.m
@@ -23,10 +23,6 @@
     _mShirtsList = [NSMutableArray array];
     _mPantsList = [NSMutableArray array];
     
-    _mEmptyView.frame = CGRectMake(0, 59, 414, 628);
-    _mEmptyImage.frame = CGRectMake(167.95, 277.34, 76, 64);
-    _mClothesCollection.frame = CGRectMake(0, 70, 414, 621);
-    
     _mEmptyView.backgroundColor = [UIColor lightGrayColor];
     
     _mEmptyImage.image = [UIImage imageNamed: @"ic_empty_list.png"];
@@ -175,7 +171,6 @@
     switch (indexPath.section) {
         case 0:
             header.mTitle.text = @"Jackets";
-            [header.mTitle sizeToFit];
             break;
             
         case 1:
@@ -217,8 +212,6 @@
     [cell.mClothesImage sd_setImageWithURL:[NSURL URLWithString: url] placeholderImage: [UIImage imageNamed: @"placeholder.png"]];
         
     cell.mTitleLabel.text = item.title;
-    [cell.mTitleLabel sizeToFit];
-    
     cell.mTitleLabel.textAlignment = NSTextAlignmentCenter;
     
     double price = item.price;
